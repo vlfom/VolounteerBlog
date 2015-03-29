135fce1576e4fa5b591143148742472b2e1eb56b
Vladimir Fomenko <vlfomenk@gmail.com>
2015-03-29 12:31:46 +0300

    Worked on design and refactored some forms

--- blogengine/forms.py
+++ blogengine/forms.py
@@ -1,7 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django import forms
 from .models import Post, User, UserProfile
 
+FIELD_NAME_MAPPING = {
+    'title': 'Назва',
+    'text': 'Текст',
+    'category': 'Категорія',
+    'tags': 'Теги',
+}
+
 class PostForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(PostForm, self).__init__(*args, **kwargs)
+        self.fields['title'].label = "Назва запису"
+        self.fields['text'].label = "Текст запису"
+        self.fields['category'].label = "Категорія"
+        self.fields['tags'].label = "Теги"
+
     class Meta:
         model = Post
         fields = ('title', 'text', 'category', 'tags')
